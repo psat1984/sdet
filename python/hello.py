b11101830df4d65334541e528d6d31fe9e25f943
psat1984 <77969592+psat1984@users.noreply.github.com>
2021-02-05 08:34:36 +0530

    Add files via upload

---
+++ python/hello.py
@@ -0,0 +1,15 @@
+print('Hello world')
+myVariable=5
+print(myVariable)
+
+
+x = 1    # int
+y = 2.8  # float
+z = 1j   # complex
+
+print(type(x)) # Output: <class 'int'>
+print(type(y)) # Output: <class 'float'>
+print(type(z)) # Output: <class 'complex'>
+
+username = input("Enter username:")
+print("Username is: " + username)
