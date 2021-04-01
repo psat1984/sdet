23ecd40efc47d31e1623c69fdceecaa8c7e2b4d3
psat1984 <77969592+psat1984@users.noreply.github.com>
2021-04-01 12:56:34 +0530

    Add files via upload

---
+++ selenium/Activity14_2.java
@@ -0,0 +1,15 @@
+package com.sdet.Selenium.Activity1;
+
+public class Activity14_2 {
+
+	public void writeExcel(String filePath) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void readExcel(String filePath) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ selenium/Activity14_2b.java
@@ -0,0 +1,17 @@
+package com.sdet.Selenium.Activity1;
+
+import java.util.List;
+
+public class Activity14_2b {
+
+	public List<List<String>> readExcel1(String filePath) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public List<List<String>> readExcel(String filePath) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
